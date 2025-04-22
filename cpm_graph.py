7bb6d75ccb013234aa78b6140ca489cdb7cd4cc5
hhc2tech <25620467+hhc2tech@users.noreply.github.com>
2025-04-22 23:21:32 +0700

    Update cpm_graph.py

--- cpm_graph.py
+++ cpm_graph.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
-def plot_gantt_chart(results, time_scale=1, language="English"):
+def plot_gantt_chart(results, time_scale=7, language="English"):
     fig, ax = plt.subplots(figsize=(14, len(results) * 0.5))
     plt.title("Gantt Chart with Critical Path", fontsize=14)
     # Format x-axis ticks
@@ -32,10 +32,11 @@ def plot_gantt_chart(results, time_scale=1, language="English"):
         ax.barh(row['Name'], duration, left=start, height=0.5, color=color, edgecolor='black')
         ax.text(start + (end - start) / 2, i, row['ID'], va='center', ha='center', color='white', fontsize=8)
 
-    ax.xaxis.set_minor_locator(minor_locator)
-    ax.xaxis.set_minor_formatter(minor_fmt)
     ax.xaxis.set_major_locator(major_locator)
     ax.xaxis.set_major_formatter(major_fmt)
+    ax.xaxis.set_minor_locator(minor_locator)
+    ax.xaxis.set_minor_formatter(minor_fmt)
+
 
     ax.tick_params(axis='x', which='major', labelsize=10, pad=10)
     ax.tick_params(axis='x', which='minor', labelsize=8, rotation=90, pad=25)
